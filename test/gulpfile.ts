553be47f92355187a37add42765f56bac4d770bd
Kath Veremeichyk <kateiiverem@gmail.com>
2022-07-18 20:42:32 +0300

    add: project with def settings

--- test/gulpfile.ts
+++ test/gulpfile.ts
@@ -1,19 +1,18 @@
 const { src, dest, watch } = require('gulp');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
 const scss = require('gulp-sass')(require('sass'));
+const minifyCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const changed = require('gulp-changed');
 
 const SCSS_SRC = './src/assets/scss/**/*.scss';
 const SCSS_DEST = './src/assets/css';
 
 function styles() {
   return src(SCSS_SRC)
-    .pipe(scss({ outputStyle: 'compressed' }))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 version'],
-      grid: true,
-    }))
+    .pipe(scss().on('error', scss.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(changed(SCSS_DEST))
     .pipe(dest(SCSS_DEST));
 }
 
